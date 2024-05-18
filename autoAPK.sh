45b9961f1620c8a3f55f52f12048791745689786
shivpratapsingh111 <shivpratapsingh777.2@gmail.com>
2024-05-18 22:03:43 +0530

    Updated

--- autoAPK.sh
+++ autoAPK.sh
@@ -19,12 +19,28 @@ for apkfile in "$dir"/*.apk; do
     if [ -f "$apkfile" ]; then
         filename=$(basename -- "$apkfile")
         foldername="${filename%.*}"
-        apktool d "$apkfile"
+        
+        echo "[x] Automation started on $filename"
 
-        # Step 2: Running nuclei on decompiled folders
-        nuclei -t mobileTemplates/ -o "$foldername"_nuclei < <(echo "$foldername")
+        (
+            apktool d "$apkfile" 
 
-        # Step 3: Running apkleaks on APK files
-        python3 /home/cyrusop/tools/apkleaks/apkleaks.py -f "$apkfile" -o "${filename%.*}"_apkleaks
+            echo "[x] $apkfile decompiled"
+        )
+
+        (
+            python3 /home/cyrusop/tools/apkleaks/apkleaks.py -f "$apkfile" -o "${filename%.*}"_apkleaks
+
+            echo "[x] Apkleaks scan finished"
+        )&
+
+        (
+            nuclei -t mobileTemplates/ -o "$foldername"_nuclei < <(echo "$foldername")
+
+            echo "[x] Nuclei scan finished"
+        )&
+
+        wait
+
     fi
 done
